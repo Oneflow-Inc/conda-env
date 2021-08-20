feab358902b10d4522784616ef04bd5b6783c279
tsai <jackalcooper@gmail.com>
2021-08-20 13:24:29 +0800

    refein

--- dev/gcc7/scripts/install-deps.sh
+++ dev/gcc7/scripts/install-deps.sh
@@ -1,2 +1,2 @@
-conda install nasm gxx_linux-64=7.3.0 cmake=3.18.2 autoconf libtool ninja sccache libopenblas-static
-conda install -c conda-forge sccache
+conda install -y nasm gxx_linux-64=7.3.0 cmake=3.18.2 autoconf libtool ninja libopenblas-static
+conda install -y -c conda-forge sccache
