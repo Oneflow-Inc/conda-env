3035fd0bff2532a7955b665d6268b64f387da421
tsai <jackalcooper@gmail.com>
2021-08-23 13:40:00 +0800

    refein

--- dev/gcc7/scripts/install-deps.sh
+++ dev/gcc7/scripts/install-deps.sh
@@ -1,4 +1,4 @@
 conda create python=3.7 --name oneflow-dev-gcc7-v2
-conda install -y -c defaults -c conda-forge nasm gxx_linux-64=7.3.0 libstdcxx-ng=7.3.0 cmake=3.18.2 autoconf libtool ninja libopenblas-static compilers sccache
+conda install -y -c defaults -c conda-forge nasm gxx_linux-64=7.3.0 libstdcxx-ng=7.3.0 libgcc-ng=7.3.0 libgomp=7.3.0 cmake=3.18.2 autoconf libtool ninja libopenblas-static compilers sccache
 conda env config vars set CXXFLAGS="-fPIC"
 conda env config vars set CFLAGS="-fPIC"
