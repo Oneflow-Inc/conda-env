fa3fbcb77d3016e2f2b4c4d1dd3fe3421c5c842d
tsai <jackalcooper@gmail.com>
2021-08-26 12:34:29 +0800

    refein

--- dev/clang10/scripts/install-deps.sh
+++ dev/clang10/scripts/install-deps.sh
@@ -1,1 +1,1 @@
-conda create --no-default-packages -n oneflow-dev-clang10-v2 -c defaults -c conda-forge python=3.8 clang=10.0.0 lld=10.0.0 cmake=3.18.2 nasm autoconf libtool ninja libopenblas-static sccache
+conda create --no-default-packages -n oneflow-dev-clang10-v2 -c defaults -c conda-forge python=3.8 clang=10.0.1 clangxx=10.0.1 lld=10.0.1 cmake=3.18.2 nasm autoconf libtool ninja libopenblas-static sccache
