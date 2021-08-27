6f3dd244cec4477b0d10b3cd947cae535e0e8840
tsai <jackalcooper@gmail.com>
2021-08-27 12:22:59 +0800

    fix

--- dev/clang10/scripts/install-deps.sh
+++ dev/clang10/scripts/install-deps.sh
@@ -1,2 +1,2 @@
-conda create --no-default-packages -n oneflow-dev-clang10-v2 -c defaults -c conda-forge python=3.8 clang=10.0.1 clangxx=10.0.1 lld=10.0.1 cmake=3.18.2 nasm autoconf libtool ninja libopenblas-static sccache
+conda create --no-default-packages -n oneflow-dev-clang10-v2 -c defaults -c conda-forge python=3.8 clang=10.0.1 clangxx=10.0.1 lld=10.0.1 cmake=3.18.2 nasm autoconf libtool automake ninja libopenblas-static sccache
 conda env config vars set CC=clang CXX=clang++
