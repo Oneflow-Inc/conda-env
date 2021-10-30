e92ab0f0c7174f0c71cf14e4db8a71c31b6ce3b0
ShawnXuan <xiexuanx2@gmail.com>
2021-10-30 23:54:34 +0800

    update

--- install/install_oneflow.sh
+++ install/install_oneflow.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
 set -x
 
-if [ "$CONDA_DEFAULT_ENV" != "base" ]; then
-  conda deactivate
-  conda activate base
-fi
+python_bin=${1:-python3}
 
-python3 -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+$python_bin -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 
 downloaded_oneflow_whl=/nfs/tools/oneflow-0.6.0.dev20211028+cu112-cp39-cp39-manylinux2014_x86_64.whl
 oneflow_whl_url=http://oneflow-static.oss-cn-beijing.aliyuncs.com/downloads/conda-installers/Miniconda3-py39_4.10.3-Linux-x86_64.sh
@@ -19,6 +15,6 @@ else
   wget ${oneflow_whl_url} -o ${downloaded_oneflow_whl}
 fi
 
-python3 -m pip install -U ${downloaded_oneflow_whl}
+$python_bin -m pip install -U ${downloaded_oneflow_whl}
 
 set +x
